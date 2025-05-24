44f4e05af1e046cf5f79bb2f4a5ecb9646026971
Gautam Patel <147582627+gauamchaudhari@users.noreply.github.com>
2025-05-24 23:00:32 +0530

    Header change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="bg-blue-600 text-white p-4 flex justify-between">
-      <h1 className="text-xl font-bold">MyShop</h1>
+      <h1 className="text-xl font-bold">Ecommserce Site</h1>
       <div>
         <Link to="/" className="mr-4 hover:underline">
           Home
